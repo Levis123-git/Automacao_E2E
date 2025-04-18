a5a944dd98334942ce1dbc75afe34c3721e12e27
Levis <alveslevi@icloud.com>
2025-04-18 02:24:52 -0300

    corrigindo o local de formatação numérica

--- src/test/java/automacaoE2E/Runner/Executa.java
+++ src/test/java/automacaoE2E/Runner/Executa.java
@@ -1,6 +1,6 @@
 package automacaoE2E.Runner;
 
-
+import org.junit.jupiter.api.BeforeAll;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,26 +8,33 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.UnreachableBrowserException;
 
 import automacaoE2E.Drivers.Drivers;
+import java.util.Locale;
 
 public class Executa extends Drivers {
-	
-	ChromeOptions options = new ChromeOptions();
-	
-	@Test
-	public void setup () {
-		try {
-			options.addArguments("--headless");
-			driver = new ChromeDriver(options);
-			driver.get("https://desafio.ui.e2etreinamentos.com.br/hb.html");
-		} catch (Exception e) {
-			throw new UnreachableBrowserException("Não foi possivel abrir o navegador");
-		}
-		
-	}
-	
-	public void tearDown() {
-		driver.quit();
-	}
-	
+    
+    ChromeOptions options = new ChromeOptions();
 
+    @BeforeAll
+    public static void setupBeforeAll() {
+        // Configura o Locale usando o método correto (não mais a string diretamente)
+        Locale.setDefault(Locale.forLanguageTag("pt-BR"));
+    }
+
+    @Test
+    public void setup() {
+        try {
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+            driver.get("https://desafio.ui.e2etreinamentos.com.br/hb.html");
+        } catch (Exception e) {
+            throw new UnreachableBrowserException("Não foi possível abrir o navegador");
+        }
+    }
+    
+    
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
